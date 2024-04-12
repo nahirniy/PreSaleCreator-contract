d0027165176a06dc02440b5f5c25e5ab5aaddf7a
nahirniy <nahirniyy@gmail.com>
2024-04-12 20:57:08 +0300

    added token and test for it

---
+++ contracts/SolarGreen.sol
@@ -0,0 +1,138 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract SolarGreen is ERC20, AccessControl {
+    bytes32 public constant BLACKLISTER = keccak256("BLACKLISTER");
+
+    uint public initiallySupply = 100000000 ether;
+
+    mapping(address => bool) private _blacklist;
+
+    event AddedToBlacklist(address indexed _address);
+    event RemovedFromBlacklist(address indexed _address);
+    event MintedNewToken(address _to, uint _amount);
+    event BurnedToken(address _to, uint _amount);
+
+    constructor(
+        address _owner,
+        address _blacklister
+    ) ERC20("Solar Green", "SGR") {
+        require(_owner != address(0), "owner address isnt correct");
+        require(!_isContract(_owner), "owner address cant be a contract");
+        require(_blacklister != address(0), "blacklister address isnt correct");
+        require(!_isContract(_blacklister), "blacklister cant be contract");
+
+        _grantRole(DEFAULT_ADMIN_ROLE, _owner);
+        _grantRole(BLACKLISTER, _blacklister);
+
+        _mint(address(this), 50000000 ether);
+    }
+
+    /**
+     * @dev Mint new tokens and allocate them to a specified account.
+     * @param _to The address where the newly minted tokens will be allocated.
+     * @param _amount The amount of tokens to mint.
+     */
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _mint(_to, _amount);
+
+        emit MintedNewToken(_to, _amount);
+    }
+
+    /**
+     * @dev Burn tokens from the contract's balance.
+     * @param _amount The amount of tokens to burn.
+     */
+    function burn(
+        address _from,
+        uint _amount
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _burn(_from, _amount);
+
+        emit BurnedToken(_from, _amount);
+    }
+
+    /**
+     * @dev Determines whether the specified address contains contract code.
+     * @param _address The address to be checked.
+     * @return True if the address contains contract code, false otherwise.
+     */
+    function _isContract(address _address) internal view returns (bool) {
+        uint256 codeSize;
+        assembly {
+            codeSize := extcodesize(_address)
+        }
+        return codeSize > 0;
+    }
+
+    /**
+     *@dev Assigns the role of a blacklister to a new address, granting authority to add addresses to the blacklist.
+     *@param _account The address to be assigned the role of a blacklister.
+     */
+    function addBlacklister(
+        address _account
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(_account != address(0), "blacklister address isnt correct");
+        require(!hasRole(BLACKLISTER, _account), "already blacklister");
+        require(!_isContract(_account), "contract address cant be blacklister");
+
+        _grantRole(BLACKLISTER, _account);
+    }
+
+    /**
+     * @dev Removes the role of a blacklister from a specified address, thereby revoking their authority to manage the blacklist.
+     * @param _account The address from which to remove the role of blacklister.
+     */
+    function removeBlacklister(
+        address _account
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(hasRole(BLACKLISTER, _account), "doesnt have role blacklister");
+
+        _revokeRole(BLACKLISTER, _account);
+    }
+
+    /**
+     * @dev Adds the specified address to the blacklist.
+     * @param _account The address to be added to the blacklist.
+     */
+    function addToBlacklist(address _account) external onlyRole(BLACKLISTER) {
+        require(!hasRole(DEFAULT_ADMIN_ROLE, _account), "admin cant be in bl");
+        require(!hasRole(BLACKLISTER, _account), "blackister cant be in bl");
+        require(!_isContract(_account), "contract address cant be in bl");
+        require(_account != address(0), "user address isnt correct");
+        require(!_blacklist[_account], "already in blacklist");
+
+        _blacklist[_account] = true;
+
+        emit AddedToBlacklist(_account);
+    }
+
+    /**
+     * @dev Removes the specified address from the blacklist.
+     * @param _account The address to be removed from the blacklist.
+     */
+    function removeFromBlacklist(
+        address _account
+    ) external onlyRole(BLACKLISTER) {
+        require(_blacklist[_account], "account isnt in the blacklist");
+
+        _blacklist[_account] = false;
+
+        emit RemovedFromBlacklist(_account);
+    }
+
+    /**
+     * @dev Checks if the specified address is blacklisted.
+     * @param _address The address to be checked.
+     * @return Whether the address is blacklisted or not.
+     */
+    function isBlacklisted(address _address) external view returns (bool) {
+        return _blacklist[_address];
+    }
+}
---
+++ test/SolarGreen.test.ts
@@ -0,0 +1,184 @@
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("SolarGreen", function () {
+  async function deploy() {
+    const [owner, buyer, spender, newOwner] = await ethers.getSigners();
+
+    const tokensForPurchase = ethers.parseUnits("50000000", 18);
+
+    const SolarGreen = await ethers.getContractFactory("SolarGreen", owner);
+    const token = await SolarGreen.deploy(owner.address, owner.address);
+
+    token.mint(owner.address, tokensForPurchase);
+
+    return { owner, buyer, spender, newOwner, token };
+  }
+
+  describe("Deployment", function () {
+    it("create token contract correct", async function () {
+      const { token, owner } = await loadFixture(deploy);
+
+      const totalMintedToken = (await token.balanceOf(token.target)) + (await token.balanceOf(owner.address));
+
+      expect(await token.totalSupply()).to.eq(totalMintedToken);
+      expect(await token.getAddress()).to.be.properAddress;
+      expect(await token.name()).to.be.eq("Solar Green");
+      expect(await token.symbol()).to.be.eq("SGR");
+
+      const DEFAULT_ADMIN_ROLE = await token.DEFAULT_ADMIN_ROLE();
+      const BLACKLISTER_ROLE = await token.BLACKLISTER();
+      expect(await token.hasRole(DEFAULT_ADMIN_ROLE, owner)).to.be.true;
+      expect(await token.hasRole(BLACKLISTER_ROLE, owner)).to.be.true;
+    });
+
+    it("should revert if owner or blacklister address is zero", async function () {
+      const [owner] = await ethers.getSigners();
+      const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+
+      const SolarGreen = await ethers.getContractFactory("SolarGreen", owner);
+
+      await expect(SolarGreen.deploy(ZERO_ADDRESS, owner.address)).to.be.revertedWith(
+        "owner address isnt correct"
+      );
+      await expect(SolarGreen.deploy(owner.address, ZERO_ADDRESS)).to.be.revertedWith(
+        "blacklister address isnt correct"
+      );
+    });
+
+    it("should revert if owner or blacklister address is a contract", async function () {
+      const [owner] = await ethers.getSigners();
+
+      const AnotherContract = await ethers.getContractFactory("TokenSaleFactory");
+      const anotherContract = await AnotherContract.deploy();
+
+      const SolarGreen = await ethers.getContractFactory("SolarGreen", owner);
+
+      await expect(SolarGreen.deploy(anotherContract, owner.address)).to.be.revertedWith(
+        "owner address cant be a contract"
+      );
+      await expect(SolarGreen.deploy(owner.address, anotherContract)).to.be.revertedWith(
+        "blacklister cant be contract"
+      );
+    });
+  });
+  describe("Token functionality", function () {
+    it("correct transfer from", async function () {
+      const { owner, buyer, spender, token } = await loadFixture(deploy);
+
+      const amount = ethers.parseUnits("8", 18);
+
+      await token.approve(spender.address, amount);
+      await token.connect(spender).transferFrom(owner.address, buyer.address, amount);
+
+      const balanceBuyer = await token.balanceOf(buyer.address);
+
+      expect(await token.allowance(owner.address, spender.address));
+      expect(balanceBuyer).to.eq(amount);
+    });
+
+    it("allow to mint new token", async function () {
+      const { owner, buyer, token } = await loadFixture(deploy);
+
+      const amount = ethers.parseUnits("10", 18);
+      const expectedTotalSupply = (await token.totalSupply()) + amount;
+
+      const tx = await token.mint(owner.address, amount);
+
+      expect(await token.totalSupply()).to.eq(expectedTotalSupply);
+      await expect(token.connect(buyer).mint(owner.address, amount)).to.be.reverted;
+      expect(tx).to.emit(token, "MintedNewToken").withArgs(owner.address, amount);
+    });
+
+    it("allow to burn token", async function () {
+      const { token } = await loadFixture(deploy);
+
+      const amount = ethers.parseUnits("8", 18);
+      const expectedTotalSupply = (await token.totalSupply()) - amount;
+
+      const tx = await token.burn(token.target, amount);
+
+      expect(await token.totalSupply()).to.eq(expectedTotalSupply);
+      expect(tx).to.emit(token, "BurnedToken").withArgs(token.target, amount);
+    });
+
+    it("only blacklister can add and remove address to blacklist", async function () {
+      const { buyer: blacklister, spender, token } = await loadFixture(deploy);
+
+      await expect(token.connect(spender).addBlacklister(blacklister.address)).to.be.reverted;
+      await token.addBlacklister(blacklister.address);
+      const firstTx = await token.connect(blacklister).addToBlacklist(spender.address);
+      expect(firstTx).to.emit(token, "AddedToBlacklist").withArgs(spender.address);
+
+      expect(await token.isBlacklisted(spender.address)).to.eq(true);
+
+      const secondTx = await token.connect(blacklister).removeFromBlacklist(spender.address);
+      expect(secondTx).to.emit(token, "removedFromBlacklist").withArgs(spender.address);
+
+      expect(await token.isBlacklisted(spender.address)).to.eq(false);
+    });
+
+    it("only owner can add and remove a blacklister", async function () {
+      const { owner, buyer: blacklister, token } = await loadFixture(deploy);
+
+      await token.addBlacklister(blacklister.address);
+
+      expect(await token.hasRole(await token.BLACKLISTER(), blacklister.address)).to.be.true;
+
+      await token.removeBlacklister(blacklister.address);
+
+      expect(await token.hasRole(await token.BLACKLISTER(), blacklister.address)).to.be.false;
+      await expect(token.connect(blacklister).addBlacklister(owner.address)).to.be.reverted;
+    });
+  });
+
+  describe("Terms and functionality of the blacklister and blacklist", async function () {
+    it("should revert if blacklister address is invalid or contract or already exists", async function () {
+      const { token, buyer: blacklister } = await loadFixture(deploy);
+      const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+      const CONTRACT_ADDRESS = token.target;
+
+      await token.addBlacklister(blacklister.address);
+
+      await expect(token.addBlacklister(ZERO_ADDRESS)).to.be.revertedWith("blacklister address isnt correct");
+      await expect(token.addBlacklister(blacklister.address)).to.be.revertedWith("already blacklister");
+      await expect(token.addBlacklister(CONTRACT_ADDRESS)).to.be.revertedWith(
+        "contract address cant be blacklister"
+      );
+    });
+    it("should revert if account is already removed from blacklister", async function () {
+      const { token, buyer: blacklister } = await loadFixture(deploy);
+
+      await token.addBlacklister(blacklister.address);
+      await token.removeBlacklister(blacklister.address);
+
+      await expect(token.removeBlacklister(blacklister.address)).to.be.revertedWith(
+        "doesnt have role blacklister"
+      );
+    });
+    it("should revertif address is admon or blacklister or contract or already in bl", async function () {
+      const { owner, token, buyer: blacklister } = await loadFixture(deploy);
+      const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+      const CONTRACT_ADDRESS = token.target;
+
+      await token.addBlacklister(blacklister.address);
+
+      await expect(token.addToBlacklist(owner.address)).to.be.revertedWith("admin cant be in bl");
+      await expect(token.addToBlacklist(blacklister.address)).to.be.revertedWith("blackister cant be in bl");
+      await expect(token.connect(blacklister).addToBlacklist(CONTRACT_ADDRESS)).to.be.revertedWith(
+        "contract address cant be in bl"
+      );
+      await expect(token.connect(blacklister).addToBlacklist(ZERO_ADDRESS)).to.be.revertedWith(
+        "user address isnt correct"
+      );
+    });
+    it("should revert if address isnt in blacklist", async function () {
+      const { token, buyer: blacklister } = await loadFixture(deploy);
+
+      await expect(token.removeFromBlacklist(blacklister.address)).to.be.revertedWith(
+        "account isnt in the blacklist"
+      );
+    });
+  });
+});
